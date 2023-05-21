d9c9137252984a5cf37b6ef7d075e5299c0e2263
pdelbecque <pdelbecque@nordnet.fr>
2023-05-21 11:04:44 +0200

    coord

--- test.py
+++ test.py
@@ -2,12 +2,12 @@ import pyautogui
 import time
 
 print ("debut")
-time.sleep(30)
+# time.sleep(10)
 x, y = pyautogui.position()
 positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
 print(positionStr, end='')
 
-pyautogui.screenshot('valider.png', region=(x, y, 180, 25))
+pyautogui.screenshot('valider.png', region=(1290, 450, 180, 25))
 print("suite")
 location = pyautogui.locateOnScreen('valider.png')
 pyautogui.click(location)
