0f11fd9d7e972c1a1f00f7b1a2f22f80b57bd9d4
pdelbecque <pdelbecque@nordnet.fr>
2023-05-21 11:09:05 +0200

    click ok

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ x, y = pyautogui.position()
 positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
 print(positionStr, end='')
 
-pyautogui.screenshot('valider.png', region=(1290, 450, 180, 25))
+pyautogui.screenshot('valider.png', region=(1300, 445, 15, 15))
 print("suite")
 location = pyautogui.locateOnScreen('valider.png')
 pyautogui.click(location)
